4de96d6a1624ac68805fea64232bc3cb4d514b9b
Kensuke Suzuki <kensuke.suzuki@asoview.co.jp>
2024-11-26 00:53:09 +0900

    fix

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -28,12 +28,12 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
-        val greeting = remember { Greeting().greet() }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!")
             }
             AnimatedVisibility(showContent) {
+                val greeting = remember { Greeting().greet() }
                 AlertDialog(
                     onDismissRequest = { showContent = false },
                     title = {
