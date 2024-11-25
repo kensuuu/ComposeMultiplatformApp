a27e3111d7a205f4e6a41d69ab0dd79c6bb647e6
Kensuke Suzuki <kensuke.suzuki@asoview.co.jp>
2024-11-26 00:41:20 +0900

    first commit

---
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -0,0 +1,60 @@
+package org.example.project
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        var showContent by remember { mutableStateOf(false) }
+        val greeting = remember { Greeting().greet() }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                AlertDialog(
+                    onDismissRequest = { showContent = false },
+                    title = {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(Icons.Default.Info, contentDescription = null)
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text("ダイアログ")
+                        }
+                    },
+                    text = {
+                        Text(greeting)
+                    },
+                    confirmButton = {
+                        Button(onClick = { showContent = false }) {
+                            Text("OK")
+                        }
+                    },
+                    backgroundColor = MaterialTheme.colors.surface,
+                    shape = MaterialTheme.shapes.medium
+                )
+            }
+        }
+    }
+}
